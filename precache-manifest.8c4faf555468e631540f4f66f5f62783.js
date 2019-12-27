c4096c30b14c803c81b7b45f7f550784e25841a0
Esse Woods <oakninja@gmail.com>
2019-12-27 23:48:20 +0100

    Updates

---
+++ precache-manifest.8c4faf555468e631540f4f66f5f62783.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b501031530e696d87f1022d26b675684",
+    "url": "/react-grayscale/index.html"
+  },
+  {
+    "revision": "4f603d7cfe53676781f8",
+    "url": "/react-grayscale/static/css/main.e7cbdd44.chunk.css"
+  },
+  {
+    "revision": "bf3845b252961e0bd2b5",
+    "url": "/react-grayscale/static/js/2.cdfe1eb2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-grayscale/static/js/2.cdfe1eb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f603d7cfe53676781f8",
+    "url": "/react-grayscale/static/js/main.e6ad20c7.chunk.js"
+  },
+  {
+    "revision": "136b79eaeeec46bed9d3",
+    "url": "/react-grayscale/static/js/runtime-main.7c1b0765.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-grayscale/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-grayscale/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-grayscale/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-grayscale/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-grayscale/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-grayscale/precache-manifest.8c4faf555468e631540f4f66f5f62783.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-grayscale/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
