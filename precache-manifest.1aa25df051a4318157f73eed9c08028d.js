6ae3423e11ae08f8cc6a178a7b313a53644a4220
Esse Woods <oakninja@gmail.com>
2019-12-28 00:33:50 +0100

    Updates

---
+++ precache-manifest.1aa25df051a4318157f73eed9c08028d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00160ff0bb7b3b3fb3eb479e4ae4afb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddfb7be2a55e204a6061",
+    "url": "/static/css/main.6ffe3d21.chunk.css"
+  },
+  {
+    "revision": "bf3845b252961e0bd2b5",
+    "url": "/static/js/2.cdfe1eb2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.cdfe1eb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddfb7be2a55e204a6061",
+    "url": "/static/js/main.f6d0160f.chunk.js"
+  },
+  {
+    "revision": "b42bf462075128365fae",
+    "url": "/static/js/runtime-main.14df7040.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-grayscale/precache-manifest.8c4faf555468e631540f4f66f5f62783.js"
+  "/precache-manifest.1aa25df051a4318157f73eed9c08028d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-grayscale/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
