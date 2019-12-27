f9ef6a73ad46714ca329f1ace8e82285a1ccc0ad
Esse Woods <oakninja@gmail.com>
2019-12-27 19:51:30 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -8,86 +8,63 @@ function App() {
 
   return (
     <div id="home">
-
       {/* Navigation */}
       <TopMenu></TopMenu>
-      {/* <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div class="container">
-          <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="#about">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> */}
 
       {/* Header */}
-      <header class="masthead">
-        <div class="container d-flex h-100 align-items-center">
-          <div class="mx-auto text-center">
-            <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
-            <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
-            <a href="#about" class="btn btn-primary js-scroll-trigger">Get Started</a>
+      <header className="masthead">
+        <div name="home" className="container d-flex h-100 align-items-center">
+          <div className="mx-auto text-center">
+            <h1 className="mx-auto my-0 text-uppercase">Grayscale</h1>
+            <h2 className="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
+            <a href="#about" className="btn btn-primary js-scroll-trigger">Get Started</a>
           </div>
         </div>
       </header>
 
       {/* About section */}
-      <section id="about" name="about" class="about-section text-center">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 mx-auto">
-              <h2 class="text-white mb-4">Built with Bootstrap 4</h2>
-              <p class="text-white-50">Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
+      <section id="about" name="about" className="about-section text-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 mx-auto">
+              <h2 className="text-white mb-4">Built with Bootstrap 4</h2>
+              <p className="text-white-50">Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
                 <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
             </div>
           </div>
-          <img src="img/ipad.png" class="img-fluid" alt=""></img>
+          <img src="img/ipad.png" className="img-fluid" alt=""></img>
         </div>
       </section>
 
       {/* Projects section */}
-      <section id="projects" name="projects" class="projects-section bg-light">
-        <div class="container">
+      <section id="projects" name="projects" className="projects-section bg-light">
+        <div className="container">
 
           {/* Featured Project Row */}
-          <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-            <div class="col-xl-8 col-lg-7">
-              <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt=""></img>
+          <div className="row align-items-center no-gutters mb-4 mb-lg-5">
+            <div className="col-xl-8 col-lg-7">
+              <img className="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt=""></img>
             </div>
-            <div class="col-xl-4 col-lg-5">
-              <div class="featured-text text-center text-lg-left">
+            <div className="col-xl-4 col-lg-5">
+              <div className="featured-text text-center text-lg-left">
                 <h4>Shoreline</h4>
-                <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                <p className="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
               </div>
             </div>
           </div>
 
           {/* Project One Row */}
-          <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
-            <div class="col-lg-6">
-              <img class="img-fluid" src="img/demo-image-01.jpg" alt=""></img>
+          <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+            <div className="col-lg-6">
+              <img className="img-fluid" src="img/demo-image-01.jpg" alt=""></img>
             </div>
-            <div class="col-lg-6">
-              <div class="bg-black text-center h-100 project">
-                <div class="d-flex h-100">
-                  <div class="project-text w-100 my-auto text-center text-lg-left">
-                    <h4 class="text-white">Misty</h4>
-                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
-                    <hr class="d-none d-lg-block mb-0 ml-0"></hr>
+            <div className="col-lg-6">
+              <div className="bg-black text-center h-100 project">
+                <div className="d-flex h-100">
+                  <div className="project-text w-100 my-auto text-center text-lg-left">
+                    <h4 className="text-white">Misty</h4>
+                    <p className="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                    <hr className="d-none d-lg-block mb-0 ml-0"></hr>
                   </div>
                 </div>
               </div>
@@ -95,17 +72,17 @@ function App() {
           </div>
 
           {/* Project Two Row */}
-          <div class="row justify-content-center no-gutters">
-            <div class="col-lg-6">
-              <img class="img-fluid" src="img/demo-image-02.jpg" alt=""></img>
+          <div className="row justify-content-center no-gutters">
+            <div className="col-lg-6">
+              <img className="img-fluid" src="img/demo-image-02.jpg" alt=""></img>
             </div>
-            <div class="col-lg-6 order-lg-first">
-              <div class="bg-black text-center h-100 project">
-                <div class="d-flex h-100">
-                  <div class="project-text w-100 my-auto text-center text-lg-right">
-                    <h4 class="text-white">Mountains</h4>
-                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
-                    <hr class="d-none d-lg-block mb-0 mr-0"></hr>
+            <div className="col-lg-6 order-lg-first">
+              <div className="bg-black text-center h-100 project">
+                <div className="d-flex h-100">
+                  <div className="project-text w-100 my-auto text-center text-lg-right">
+                    <h4 className="text-white">Mountains</h4>
+                    <p className="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                    <hr className="d-none d-lg-block mb-0 mr-0"></hr>
                   </div>
                 </div>
               </div>
@@ -116,17 +93,17 @@ function App() {
       </section>
 
       {/* <!-- Signup Section --> */}
-      <section id="signup" name="signup" class="signup-section">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-10 col-lg-8 mx-auto text-center">
+      <section id="signup" name="signup" className="signup-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 col-lg-8 mx-auto text-center">
 
-              <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-              <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
+              <i className="far fa-paper-plane fa-2x mb-2 text-white"></i>
+              <h2 className="text-white mb-5">Subscribe to receive updates!</h2>
 
-              <form class="form-inline d-flex">
-                <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address..."></input>
-                <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
+              <form className="form-inline d-flex">
+                <input type="email" className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address..."></input>
+                <button type="submit" className="btn btn-primary mx-auto">Subscribe</button>
               </form>
 
             </div>
@@ -135,56 +112,56 @@ function App() {
       </section>
 
       {/* <!-- Contact Section --> */}
-      <section name="contact" class="contact-section bg-black">
-        <div class="container">
+      <section name="contact" className="contact-section bg-black">
+        <div className="container">
 
-          <div class="row">
+          <div className="row">
 
-            <div class="col-md-4 mb-3 mb-md-0">
-              <div class="card py-4 h-100">
-                <div class="card-body text-center">
-                  <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                  <h4 class="text-uppercase m-0">Address</h4>
-                  <hr class="my-4"></hr>
-                  <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+            <div className="col-md-4 mb-3 mb-md-0">
+              <div className="card py-4 h-100">
+                <div className="card-body text-center">
+                  <i className="fas fa-map-marked-alt text-primary mb-2"></i>
+                  <h4 className="text-uppercase m-0">Address</h4>
+                  <hr className="my-4"></hr>
+                  <div className="small text-black-50">4923 Market Street, Orlando FL</div>
                 </div>
               </div>
             </div>
 
-            <div class="col-md-4 mb-3 mb-md-0">
-              <div class="card py-4 h-100">
-                <div class="card-body text-center">
-                  <i class="fas fa-envelope text-primary mb-2"></i>
-                  <h4 class="text-uppercase m-0">Email</h4>
-                  <hr class="my-4"></hr>
-                  <div class="small text-black-50">
+            <div className="col-md-4 mb-3 mb-md-0">
+              <div className="card py-4 h-100">
+                <div className="card-body text-center">
+                  <i className="fas fa-envelope text-primary mb-2"></i>
+                  <h4 className="text-uppercase m-0">Email</h4>
+                  <hr className="my-4"></hr>
+                  <div className="small text-black-50">
                     <a href="#">hello@yourdomain.com</a>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class="col-md-4 mb-3 mb-md-0">
-              <div class="card py-4 h-100">
-                <div class="card-body text-center">
-                  <i class="fas fa-mobile-alt text-primary mb-2"></i>
-                  <h4 class="text-uppercase m-0">Phone</h4>
-                  <hr class="my-4"></hr>
-                  <div class="small text-black-50">+1 (555) 902-8832</div>
+            <div className="col-md-4 mb-3 mb-md-0">
+              <div className="card py-4 h-100">
+                <div className="card-body text-center">
+                  <i className="fas fa-mobile-alt text-primary mb-2"></i>
+                  <h4 className="text-uppercase m-0">Phone</h4>
+                  <hr className="my-4"></hr>
+                  <div className="small text-black-50">+1 (555) 902-8832</div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="social d-flex justify-content-center">
-            <a href="#" class="mx-2">
-              <i class="fab fa-twitter"></i>
+          <div className="social d-flex justify-content-center">
+            <a href="#" className="mx-2">
+              <i className="fab fa-twitter"></i>
             </a>
-            <a href="#" class="mx-2">
-              <i class="fab fa-facebook-f"></i>
+            <a href="#" className="mx-2">
+              <i className="fab fa-facebook-f"></i>
             </a>
-            <a href="#" class="mx-2">
-              <i class="fab fa-github"></i>
+            <a href="#" className="mx-2">
+              <i className="fab fa-github"></i>
             </a>
           </div>
 
@@ -192,8 +169,8 @@ function App() {
       </section>
 
       {/* <!-- Footer --> */}
-      <footer class="bg-black small text-center text-white-50">
-        <div class="container">
+      <footer className="bg-black small text-center text-white-50">
+        <div className="container">
           Copyright &copy; Your Website 2019
         </div>
       </footer>
--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -2,21 +2,49 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import * as Scroll from 'react-scroll';
 
+
 class TopMenu extends React.Component {
+    constructor (props){
+        super(props);
+        this.scrollToTop = this.scrollToTop.bind(this);
+    }
+    
+    componentDidMount() {
+    
+      Scroll.Events.scrollEvent.register('begin', function() {
+        console.log("begin", arguments);
+      });
+    
+      Scroll.Events.scrollEvent.register('end', function() {
+        console.log("end", arguments);
+      });
+    
+      Scroll.scrollSpy.update();
+    }
+    scrollToTop(options) {
+      Scroll.animateScroll.scrollToTop(options);
+    }
+    componentWillUnmount() {
+      Scroll.Events.scrollEvent.remove('begin');
+      Scroll.Events.scrollEvent.remove('end');
+    }
+
     render() {
-      return <Navbar bg="white" fixed="top" expand="sm"> {/* missing id mainNav */}
-      <Container>
-        <Navbar.Brand href="#home" onClick={() => Scroll.animateScroll.scrollToTop()}>React-GrayScale</Navbar.Brand> 
-        <Navbar.Toggle class="navbar-toggler-right" aria-controls="responsive-navbar-nav" aria-expanded="false" aria-label="Toggle navigation" />
-        <Navbar.Collapse id="navbarResponsive">
-          <Nav className="ml-auto">
-            <Scroll.Link smooth={true} duration={500} spy={true} className="nav-link" href="#about" to="about">About</Scroll.Link>
-            <Scroll.Link smooth={true} duration={500} spy={true} className="nav-link" href="#projects" to="projects">Projects</Scroll.Link>
-            <Scroll.Link smooth={true} duration={500} spy={true} className="nav-link" href="#signup" to="signup">Contact</Scroll.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      return <Navbar fixed="top" expand="sm"> {/* missing id mainNav */}
+                <Container>
+                    <Navbar.Brand>
+                        <Scroll.Link className="nav-link" smooth={true} duration={500} spy={true} href="#home" to="home">React-GrayScale</Scroll.Link>
+                    </Navbar.Brand> 
+                    <Navbar.Toggle className="navbar-toggler-right" aria-controls="responsive-navbar-nav" aria-expanded="false" aria-label="Toggle navigation" />
+                    <Navbar.Collapse id="navbarResponsive">
+                    <Nav className="ml-auto">
+                        <Scroll.Link smooth={true} duration={500} spy={true} className="nav-link" href="#about" to="about">About</Scroll.Link>
+                        <Scroll.Link smooth={true} duration={500} spy={true} className="nav-link" href="#projects" to="projects">Projects</Scroll.Link>
+                        <Scroll.Link smooth={true} duration={500} spy={true} className="nav-link" href="#signup" to="signup">Contact</Scroll.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+             </Navbar>
     }
   }
 
